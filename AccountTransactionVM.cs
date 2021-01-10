03b3fc2464f232500a80e63c9fb1daaa5a2b15c2
Eshtayah <abualyosr96@hotmail.com>
2021-01-10 11:59:29 +0200

    first push

---
+++ AccountTransactionVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PEGASE.Domain.ViewModels
+{
+    public class AccountTransactionVM  
+    { 
+        public long ID { get; set; } 
+        public DateTime TransDate { get; set; } 
+        public DateTime ValueDate { get; set; } 
+        public long AccountID { get; set; }
+        public string TransDesc { get; set; } 
+        public decimal AccountBalance { get; set; } 
+    }
+}
---
+++ CSPPayrollVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PEGASE.Domain.ViewModels
+{
+    public class CSPPayrollVM  
+    {
+         
+        public long ID { get; set; } 
+        public long DatasetRequestID { get; set; } 
+        public long EmpNo { get; set; } 
+        public string FirstNameAr { get; set; } 
+        public string FatherNameAr { get; set; } 
+        public string GranFatherNameAr { get; set; } 
+        
+    }
+}
